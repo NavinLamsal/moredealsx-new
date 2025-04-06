4bad9444df6fd2f425487bcd591c9d902d14b5b1
navinlamsal <navinlamsal11@gmail.com>
2025-04-06 13:45:59 +0545

    theme error

--- src/components/HOC/provider.tsx
+++ src/components/HOC/provider.tsx
@@ -9,7 +9,7 @@ const Provider = ({ children }: { children: ReactNode }) => {
     return (
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
         >
