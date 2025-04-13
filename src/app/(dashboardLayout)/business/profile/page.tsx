3dbc17fae337e544afe0afd3ccdfe07a77ee1644
unknown <anju79090@gmail.com>
2025-04-13 12:44:05 +0545

    Update

--- src/app/(dashboardLayout)/business/profile/page.tsx
+++ src/app/(dashboardLayout)/business/profile/page.tsx
@@ -1,28 +1,31 @@
-import BusinessContent from '@/components/settings/BusinessContent'
-import BusinessProfileCard from '@/components/settings/BusinessProfileCard'
-import BusinessQuickLinks from '@/components/settings/BusinessQuickLinks'
+import BusinessContent from "@/components/settings/BusinessContent";
+import BusinessProfileCard from "@/components/settings/BusinessProfileCard";
+import BusinessQuickLinks from "@/components/settings/BusinessQuickLinks";
+import BackButton from "@/components/ui/back_button";
 
-import { Button } from '@/components/ui/button'
-import Heading from '@/components/ui/heading'
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
 
-import { Edit2, } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import { Edit2 } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
-    <div className='p-4 '>
-    
-    <div className='flex flex-row justify-between'>
+    <div className="p-4 ">
+      <div className="flex flex-row justify-between">
+        <BackButton />
         <Heading title="Business Profile" />
         {/* <h1 className='text-3xl font-bold'>Business Profile</h1> */}
-        <Link href={'/business/profile/update'}>
-            <Button size={"icon"}><Edit2 className=''/></Button>
+        <Link href={"/business/profile/update"}>
+          <Button size={"icon"}>
+            <Edit2 className="" />
+          </Button>
         </Link>
+      </div>
+      <BusinessContent />
     </div>
-    <BusinessContent/>
-    </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
