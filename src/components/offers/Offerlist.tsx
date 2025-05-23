600b2d02962db81769193e7a0339d2829a3f56c0
navinlamsal <navinlamsal11@gmail.com>
2025-05-23 10:02:23 +0545

    offers

--- src/components/offers/Offerlist.tsx
+++ src/components/offers/Offerlist.tsx
@@ -179,8 +179,6 @@ const AllOffersList = () => {
         <div className="max-w-6xl mx-auto px-4 py-8">
             {/* No Transactions Found */}
             {data?.pages[0].data.length === 0 && <p className="text-center">No Offers Found</p>}
-             <UniversalTextLoading messages={["Creating your account", "Creating Wallet for you...", "Almost finishing Up...", "Finishing Up..."]} pauseTime={10000} speed={200} />
-
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
                 {data?.pages.map((page: any) =>
                     page?.data.map((offer: Offer, index: number) => (
