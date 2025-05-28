c69d54c45c8f0dad0bce0f9418793153b9c59ec1
Angela Basnet <angellabasnet@gmail.com>
2025-05-28 12:52:04 +0545

    fixed cat selection overflow

--- src/components/offers/OfferCategory.tsx
+++ src/components/offers/OfferCategory.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
 import React from "react";
 import { cn } from "@/lib/utils"; // adjust path as needed
 import { useSearchParams, useRouter } from "next/navigation";
 
 interface CategorySelectorProps {
-  categories: {title: string, value: string}[];
+  categories: { title: string; value: string }[];
   activeCategory?: string;
   onCategoryChange?: (category: string) => void;
   UseSearchParams?: boolean;
@@ -39,37 +39,34 @@ const CategorySelector: React.FC<CategorySelectorProps> = ({
     : activeCategory;
 
   return (
-  
     <div
-  className={cn(
-    "w-full overflow-x-auto mb-10",
-    !dashboardStyle && "flex justify-center" // center scroll container if not dashboard
-  )}
->
-  <div
-    className={cn(
-      "inline-flex gap-4 px-2", // no wrapping, horizontally scrollable
-      dashboardStyle ? "justify-start" : "justify-center"
-    )}
-  >
-    {categories.map((category) => (
-      <button
-        key={category.value}
-        onClick={() => handleClick(category.value)}
+      className={cn(
+        "w-full overflow-x-auto mb-10 min-[660px]:pl-0 min-[570px]:pl-20 min-[460px]:pl-40 pl-64",
+        !dashboardStyle && "flex justify-center" // center scroll container if not dashboard
+      )}
+    >
+      <div
         className={cn(
-          "px-5 py-2 font-extrabold text-sm border-b-4 transition-all whitespace-nowrap",
-          selectedCategory === category.value
-            ? "text-yellow-400 border-yellow-400"
-            :  "text-foreground border-transparent hover:text-yellow-400 hover:border-yellow-400"
+          "inline-flex gap-4 px-2", // no wrapping, horizontally scrollable
+          dashboardStyle ? "justify-start" : "justify-center"
         )}
       >
-        {category.title}
-      </button>
-    ))}
-  </div>
-</div>
-
-
+        {categories.map((category) => (
+          <button
+            key={category.value}
+            onClick={() => handleClick(category.value)}
+            className={cn(
+              "px-5 py-2 font-extrabold text-sm border-b-4 transition-all whitespace-nowrap",
+              selectedCategory === category.value
+                ? "text-yellow-400 border-yellow-400"
+                : "text-foreground border-transparent hover:text-yellow-400 hover:border-yellow-400"
+            )}
+          >
+            {category.title}
+          </button>
+        ))}
+      </div>
+    </div>
   );
 };
 
