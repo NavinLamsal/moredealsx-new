f149b58b63a2c598cca9c602576a1a698061ff71
navinlamsal <navinlamsal11@gmail.com>
2025-05-28 10:16:33 +0545

    google login

--- src/components/auth/GoogleLoginComponent.tsx
+++ src/components/auth/GoogleLoginComponent.tsx
@@ -98,7 +98,9 @@ const GoogleLoginButton: React.FC = () => {
       >
         G
       </button> */}
-      <div className="w-10 h-10 rounded-full bg-black text-yellow-400 border border-yellow-400 flex items-center justify-center text-lg cursor-pointer transition-all hover:bg-yellow-400 hover:text-black hover:-translate-y-1">
+      <div className="w-10 h-10 rounded-full bg-black text-yellow-400 border border-yellow-400 flex items-center justify-center text-lg cursor-pointer transition-all hover:bg-yellow-400 hover:text-black hover:-translate-y-1"
+      onClick = {() => login()}
+      >
         <Image
           src={"/images/svg/google.svg"}
           alt="google logo"
