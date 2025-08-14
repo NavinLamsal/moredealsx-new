6f7a367a1c33bb1987eb93691c0f1f9eac1a134a
navin lamsal <navinlamsal11@gmail.com>
2025-08-14 11:27:26 +0545

    cookie problem

--- src/lib/utils/access.ts
+++ src/lib/utils/access.ts
@@ -4,7 +4,7 @@ import { setCookie } from "cookies-next/client";
 export function setTokenCookie(name:string, token: string , age: number = 60 * 60) {
     console.log(`Setting cookie: ${name} with token: ${token}`, process.env.NODE_ENV);
     return setCookie(name, token, {
-    httpOnly: process.env.NODE_ENV === 'production',
+    httpOnly: false,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
     path: '/',
